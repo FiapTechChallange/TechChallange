56144e3abb717f9f94792c3c9ab5c33347ce4aea
Gabriel Maximiniano <gabriel.maximiniano@vitta.me>
2023-09-01 21:25:14 -0300

    add: volume database

--- Makefile
+++ Makefile
@@ -5,3 +5,7 @@ start-kubernetes:
 
 delete-namespace:
 	kubectl delete all --all --namespace tech-challenge
+
+
+start-db:
+	kubectl exec -i deploy/database -n tech-challenge -- mysql -u root -ppassword FIAP_CHALLENGE < dump-database.sql
---
+++ dump-database.sql
@@ -0,0 +1,223 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Tempo de geração: 02/09/2023 às 00:40
+-- Versão do servidor: 8.1.0
+-- Versão do PHP: 8.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `FIAP_CHALLENGE`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cardapio`
+--
+
+CREATE TABLE `cardapio` (
+  `id` int NOT NULL,
+  `nome` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `descricao` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `id_categoria` int NOT NULL,
+  `valor` decimal(6,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id` int NOT NULL,
+  `nome` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `nome`) VALUES
+(1, 'Lanche'),
+(2, 'Acompanhamento'),
+(3, 'Bebida'),
+(4, 'Sobremesa');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int NOT NULL,
+  `cpf` varchar(64) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nome` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `telefone` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `id` int NOT NULL,
+  `id_cliente` int DEFAULT NULL,
+  `fechamento` datetime DEFAULT NULL,
+  `pagamento` datetime DEFAULT NULL,
+  `status` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'iniciado'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pedido_itens`
+--
+
+CREATE TABLE `pedido_itens` (
+  `id` int NOT NULL,
+  `id_pedido` int NOT NULL,
+  `id_item_cardapio` int DEFAULT NULL,
+  `observacoes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `preparo`
+--
+
+CREATE TABLE `preparo` (
+  `id` int NOT NULL,
+  `id_pedido` int NOT NULL,
+  `inicio` datetime NOT NULL,
+  `termino` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cardapio`
+--
+ALTER TABLE `cardapio`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cardapio_categoria_id_fk` (`id_categoria`);
+
+--
+-- Índices de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `pedido_itens`
+--
+ALTER TABLE `pedido_itens`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pedido_itens_pedido_id_fk` (`id_pedido`),
+  ADD KEY `pedido_itens_cardapio_id_fk` (`id_item_cardapio`);
+
+--
+-- Índices de tabela `preparo`
+--
+ALTER TABLE `preparo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `preparo_pedido_id_fk` (`id_pedido`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cardapio`
+--
+ALTER TABLE `cardapio`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pedido_itens`
+--
+ALTER TABLE `pedido_itens`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `preparo`
+--
+ALTER TABLE `preparo`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `cardapio`
+--
+ALTER TABLE `cardapio`
+  ADD CONSTRAINT `cardapio_categoria_id_fk` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`);
+
+--
+-- Restrições para tabelas `pedido_itens`
+--
+ALTER TABLE `pedido_itens`
+  ADD CONSTRAINT `pedido_itens_cardapio_id_fk` FOREIGN KEY (`id_item_cardapio`) REFERENCES `cardapio` (`id`),
+  ADD CONSTRAINT `pedido_itens_pedido_id_fk` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`);
+
+--
+-- Restrições para tabelas `preparo`
+--
+ALTER TABLE `preparo`
+  ADD CONSTRAINT `preparo_pedido_id_fk` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
